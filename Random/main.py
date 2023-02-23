236cf3207a6c90312146707f231c2efc566024ef
lucifer-14 <killerlucifer14@gmail.com>
2023-02-23 17:12:53 +0630

    changed tts

--- Random/main.py
+++ Random/main.py
@@ -4,6 +4,7 @@ import os
 from dotenv import load_dotenv
 import image_generator as ig
 import chat as c
+import youtube_dl
 
 load_dotenv()
 
@@ -13,6 +14,32 @@ CHATGPT_TOKEN = os.getenv('CHATGPT_TOKEN')
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 # bot.channel bot.channels.cache.get("ChannelID")
+
+
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0',  # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn',
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+
 
 @bot.event
 async def on_ready():
@@ -31,12 +58,20 @@ async def chat(ctx, prompt):
     await ctx.send(await c.chatgpt(OPENAI_TOKEN, prompt))
 
 @bot.command()
+async def chat_tts(ctx, prompt):
+    await ctx.send(await c.chatgpt(OPENAI_TOKEN, prompt), tts=True)
+
+@bot.command()
 async def ping(ctx):
     await ctx.send("Were you expecting something?\nIt's me, Pong!")
 
 @bot.command()
 async def command_list(ctx):
     await ctx.send("!ping\n!chat \"What is token?\"\n!image \"Some cool image\"")
+
+# @bot.command()
+# async def slash_test(ctx):
+#     await ctx.send("`lol`")
 
 @bot.command()
 async def join(ctx):
@@ -52,5 +87,14 @@ async def join(ctx):
 async def leave(ctx):
     await ctx.voice_client.disconnect()
 
+
+@bot.command()
+async def play(ctx, query):
+    source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(query))
+    ctx.voice_client.play(source, after=lambda e: print(f'Player error: {e}') if e else None)
+
+# @bot.command()
+# async def test(ctx):
+#     await ctx.send("/tts hello", tts=True)
 
 bot.run(BOT_TOKEN)
